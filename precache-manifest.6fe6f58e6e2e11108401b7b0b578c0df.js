b54bd646cfac1e90531d8cb12f3003a95e101f79
unknown <ma.sander11@gmail.com>
2019-04-06 11:52:12 -0300

    Updates

---
+++ precache-manifest.6fe6f58e6e2e11108401b7b0b578c0df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TestesReact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "65b20f0aa99ce13db549",
+    "url": "/TestesReact/static/js/runtime~main.b77cc0ee.js"
+  },
+  {
+    "revision": "40338410852ed3bc1daa",
+    "url": "/TestesReact/static/js/main.750d2c1b.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/TestesReact/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "40338410852ed3bc1daa",
+    "url": "/TestesReact/static/css/main.318784b5.chunk.css"
+  },
+  {
+    "revision": "a40ce6c5fb95a4f358aa6efa589312b4",
+    "url": "/TestesReact/index.html"
+  }
+];
